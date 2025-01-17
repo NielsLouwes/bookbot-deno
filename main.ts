2627025eaa1fd82f7c71900205b1bbb4d7ff87d1
Niels Louwes <nielslouwes45@gmail.com>
2025-01-17 11:24:26 +0100

    readfile and find story start

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
 const text = await Deno.readTextFile("./books/frankenstein.txt");
 console.log("text", text);
 
-// step 1 - count words
+// step 1 - count words - but don't count the extra text before the story
+
+const findStartOfStory = (text) => {
+  const storyText = text.split("\n");
+  const preparedText = storyText.filter((line) => line.trim() !== "");
+
+  console.log("preparedText", preparedText);
+
+  const find = storyText.findIndex((word) => word == "Letter 1");
+  console.log("find", find);
+};
+
+findStartOfStory(text);
