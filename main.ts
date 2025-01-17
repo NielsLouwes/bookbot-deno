241bc23d5e2bad8682071c2b81545f0c92d3e75e
Niels Louwes <nielslouwes45@gmail.com>
2025-01-17 11:40:10 +0100

    bookbot: find start and end story

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 const text = await Deno.readTextFile("./books/frankenstein.txt");
-console.log("text", text);
 
-// step 1 - count words - but don't count the extra text before the story
+// step 1 - prepare text and find start / end of story for counting
 
 const findStartOfStory = (text) => {
   const storyText = text.split("\n");
-  const preparedText = storyText.filter((line) => line.trim() !== "");
+  const preparedText = storyText
+    .filter((line) => line.trim() !== "")
+    .flatMap((line) => line.trim().split(/\s+/));
 
-  console.log("preparedText", preparedText);
+  const story = preparedText.slice(4);
 
-  const find = storyText.findIndex((word) => word == "Letter 1");
-  console.log("find", find);
+  return story;
 };
 
-findStartOfStory(text);
+let storyText = findStartOfStory(text);
+
+console.log("STORY:", storyText);
+
+// step 2 - count words - but don't count the extra text before the story
+
+const countChars = () => {};
---
+++ todo.md
@@ -0,0 +1,5 @@
+1. Find total characters count
+2. find count per letter
+3. find count of all vowels
+4. Return most common letters
+5. Run this from CLI
