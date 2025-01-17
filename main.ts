0a236290f32169cd080bb539c550cdc656fab5b9
Niels Louwes <nielslouwes45@gmail.com>
2025-01-17 16:54:34 +0100

    solution most occuring letters

--- main.ts
+++ main.ts
@@ -29,3 +29,33 @@ const countChars = (text: string[]): number => {
 
 const storyCharCount = countChars(storyText);
 console.log("Total chars in this story:", storyCharCount);
+
+// step 3: count occurence of each letter in the story
+
+const returnLetterCount = (text: string[]) => {
+  const chars = text.map((word) => word.split("")).flat();
+
+  let letterObject = {};
+
+  chars.map((char: string) => {
+    if (!letterObject[char]) {
+      letterObject[char] = 0;
+    }
+
+    letterObject[char]++;
+  });
+
+  const sortedLettersByOccurence = Object.fromEntries(
+    Object.entries(letterObject)
+      .sort(([, a], [, b]) => b - a)
+      .slice(0, 3)
+  );
+
+  const topLetters = Object.keys(sortedLettersByOccurence);
+  const [first, second, third] = topLetters;
+  return `The most occuring letters are ${first},${second},${third}.`;
+};
+
+const topThreeLetters = returnLetterCount(storyText);
+
+console.log(topThreeLetters);
