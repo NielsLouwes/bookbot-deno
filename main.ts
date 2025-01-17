26ad72b7356e07f13b82964843c607f7b6d6c92b
Niels Louwes <nielslouwes45@gmail.com>
2025-01-17 12:01:52 +0100

    adds char count

--- main.ts
+++ main.ts
@@ -6,7 +6,8 @@ const findStartOfStory = (text) => {
   const storyText = text.split("\n");
   const preparedText = storyText
     .filter((line) => line.trim() !== "")
-    .flatMap((line) => line.trim().split(/\s+/));
+    .flatMap((line) => line.trim().split(/\s+/))
+    .map((word) => word.replace(/[^a-zA-Z]/g, "").toLowerCase());
 
   const story = preparedText.slice(4);
 
@@ -15,8 +16,16 @@ const findStartOfStory = (text) => {
 
 let storyText = findStartOfStory(text);
 
-console.log("STORY:", storyText);
+// console.log("STORY:", storyText);
 
 // step 2 - count words - but don't count the extra text before the story
 
-const countChars = () => {};
+const countChars = (text) => {
+  const chars = text.map((word) => word.split(""));
+  const length = chars.length;
+
+  return length;
+};
+
+const storyCharCount = countChars(storyText);
+console.log("Total chars in this story:", storyCharCount);
