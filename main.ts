1ce2622384e4b1b7fb788265089333e7c39c4edf
Niels Louwes <nielslouwes45@gmail.com>
2025-01-17 12:05:18 +0100

    added types

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ const text = await Deno.readTextFile("./books/frankenstein.txt");
 
 // step 1 - prepare text and find start / end of story for counting
 
-const findStartOfStory = (text) => {
+const findStartOfStory = (text: string) => {
   const storyText = text.split("\n");
   const preparedText = storyText
     .filter((line) => line.trim() !== "")
@@ -20,7 +20,7 @@ let storyText = findStartOfStory(text);
 
 // step 2 - count words - but don't count the extra text before the story
 
-const countChars = (text) => {
+const countChars = (text: string[]): number => {
   const chars = text.map((word) => word.split(""));
   const length = chars.length;
 
